aecec8c5ea23541540e80e2de6390c14297d25cb
Marek Kobida <marek.kobida@gmail.com>
2023-02-10 12:23:49 +0100

    prerábka

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
-const saveEl = document.getElementById('save-el');
 const countEl = document.getElementById('count-el');
+const logsEl = document.getElementById('logs-el');
+
+const decrementBtn = document.getElementById('decrement-btn');
+const incrementBtn = document.getElementById('increment-btn');
+const refreshBtn = document.getElementById('refresh-btn');
+const saveBtn = document.getElementById('save-btn');
+
 let count = 0;
+
+function renderCount() {
+  countEl.textContent = count;
+}
 
 function increment() {
   count += 1;
-  countEl.textContent = count;
+  renderCount();
 }
 
 function decrement() {
   count -= 1;
-  countEl.textContent = count;
+  renderCount();
 }
 
 function save() {
-  const countStr = count + ' - ';
-  saveEl.textContent += countStr;
-  countEl.textContent = 0;
+  logsEl.textContent += count + ' - ';
+
   count = 0;
+  renderCount();
 }
 
 function refresh() {
-  countEl.innerText = 0;
-  saveEl.innerText = 'Logs: ';
+  logsEl.innerText = 'Logs: ';
+
   count = 0;
+  renderCount();
 }
+
+/**/
+
+decrementBtn.addEventListener('click', decrement);
+incrementBtn.addEventListener('click', increment);
+refreshBtn.addEventListener('click', refresh);
+saveBtn.addEventListener('click', save);
