de9dc34871fb3afdbb78160112a946d43a5e7b81
Marek Kobida <marek.kobida@gmail.com>
2023-02-10 12:17:30 +0100

    linter applied

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
-
+const saveEl = document.getElementById('save-el');
+const countEl = document.getElementById('count-el');
+let count = 0;
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+  count += 1;
+  countEl.textContent = count;
 }
 
-
 function decrement() {
-    count -= 1
-    countEl.textContent = count
-
+  count -= 1;
+  countEl.textContent = count;
 }
 
 function save() {
-    
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+  const countStr = count + ' - ';
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
 }
 
-
 function refresh() {
-    countEl.innerText = 0;
-    saveEl.innerText = 'Logs: ';
-    count = 0
+  countEl.innerText = 0;
+  saveEl.innerText = 'Logs: ';
+  count = 0;
 }
